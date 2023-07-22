ff85c94b1a22e998a3b27f40236ee1a9358ee366
nekone <nekonekun@gmail.com>
2023-07-22 21:58:12 +0400

    fix(db): Fix alembic-script command-line arguments to match main script arguments

--- src/digest/db/entrypoint.py
+++ src/digest/db/entrypoint.py
@@ -11,12 +11,45 @@ def main():
     """Alembic entrypoint"""
     alembic = CommandLine()
     alembic.parser.add_argument(
-        '--database',
-        default=os.getenv('DIGEST_DATABASE_URL'),
-        help='Database URL [env var: DIGEST_DATABASE_URL]',
+        '--postgres-username',
+        help='Postgres username',
+        metavar='user',
+        default=os.environ.get('DIGEST_POSTGRES_USERNAME'),
+    )
+    alembic.parser.add_argument(
+        '--postgres-password',
+        help='Postgres password',
+        metavar='pass',
+        default=os.environ.get('DIGEST_POSTGRES_PASSWORD'),
+    )
+    alembic.parser.add_argument(
+        '--postgres-host',
+        help='Postgres host',
+        metavar='host',
+        default=os.environ.get('DIGEST_POSTGRES_HOST', 'localhost'),
+    )
+    alembic.parser.add_argument(
+        '--postgres-port',
+        help='Postgres port',
+        metavar='5432',
+        default=os.environ.get('DIGEST_POSTGRES_PORT', 5432),
+    )
+    alembic.parser.add_argument(
+        '--postgres-database',
+        help='Postgres database',
+        metavar='db',
+        default=os.environ.get('DIGEST_POSTGRES_DATABASE'),
+    )
+    options = alembic.parser.parse_args()
+    username = options.postgres_username
+    password = options.postgres_password
+    host = options.postgres_host
+    port = options.postgres_port
+    db = options.postgres_database
+    options.database = (
+        f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{db}'
     )
 
-    options = alembic.parser.parse_args()
     if not os.path.isabs(options.config):
         options.config = os.path.join(PROJECT_PATH, options.config)
 
